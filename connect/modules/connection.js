fc4c7baab9aa427198e57d0a6973875441a4bb32
lpaschopoulos <lpaschopoulos@gmail.com>
2023-07-03 21:18:52 +0300

    up

--- connect/modules/connection.js
+++ connect/modules/connection.js
@@ -1,21 +1,12 @@
 const mongoose = require("mongoose");
-require ("dotenv").config();
+require("dotenv").config();
 const uri = process.env.MONGODB_URI;
 
-
-const connectionParams={
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
-
-async function db() {
-  try {
-    await mongoose.connect(uri, connectionParams);
-    console.log("Connected to database.");
-  } catch (error) {
-    console.log("Could not connect to database.", error);
-  }
-}
+const db = mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = db;
+
 
