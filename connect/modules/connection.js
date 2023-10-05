7d996bad95f8e4743eeb728433124f15a7d7d901
lpaschopoulos <lpaschopoulos@gmail.com>
2023-10-05 19:53:46 +0300

    connection uri

--- connect/modules/connection.js
+++ connect/modules/connection.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const uri = process.env.MONGODB_URI;
 
-const db = mongoose.connect('mongodb+srv://loukas_pas:YBBMHPAAmVGTM3Kq@cluster0.ahcnc1p.mongodb.net/skillconnect?retryWrites=true&w=majority'
-, {
+const db = mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 module.exports = db;
-
-
